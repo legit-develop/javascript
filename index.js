7ce49a9def67f7592fc2eca7186a9dc8758540b2
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-06-15 14:44:17 +0200

    dynamic object combine with unique values

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
-//Dynamic Object Key
+const menu = [
+  {
+    name: "pancakes",
+    category: "breakfast",
+  },
+  {
+    name: "burger",
+    category: "lunch",
+  },
+  {
+    name: "steak",
+    category: "dinner",
+  },
+  {
+    name: "bacon",
+    category: "breakfast",
+  },
+];
 
-const person = {
-  name: "Sifiso",
-};
-
-//Dot Notation
-console.log(person.name);
-person.age = 25;
-console.log(person);
-
-//Square Bracket Notation
-const items = {
-  "features-items": ["items 1", "item 2"],
-};
-console.log(items["features-items"]);
-
-let appState = "loading";
-appState = "error";
-const keyName = "computer";
-
-const app = {
-  [appState]: true,
-};
-
-app[keyName] = "apple";
-console.log(app);
-
-const state = {
-  loading: true,
-  name: "",
-  job: "",
-};
-
-function updateState(key, value) {
-  state[key] = value;
-}
-
-updateState("loading", false);
-updateState("name", "Dhlamini");
-updateState("job", "full stack developer");
-console.log(state);
-
-let appCon = "loading";
-
-const appL = {
-  [appCon]: true,
-};
-console.log(appL);
+const category = menu.map((items) => items.category);
+console.log(category);
+const uniCategory = [...new Set(category)];
+console.log(uniCategory);
